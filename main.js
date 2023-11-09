457232f47bf7631024a6f9d5a83ddb81385a5327
misrar09 <misrar09@gmail.com>
2023-11-09 09:20:59 +0100

    refacotring random numbers

--- main.js
+++ main.js
@@ -1,8 +1,5 @@
 
-
-
 // create a 10x10 grid 
-
 const grid = document.getElementById("grid");
 
 // Showing the grid by selecting the deficulting level and clicking on the button
@@ -58,16 +55,13 @@ function createSquareDiv(className, squareNum, numbers) {
             console.log("box number is:", squareNum + numbers);
             squareDiv.classList.toggle("red");
         } else {
-            console.log("box number is:", squareNum);
+            console.log("box number is:", squareNum;
             squareDiv.classList.toggle("blue");
         }
     });
 
     return squareDiv;
 }
-
-
-
 
 //Generate 16 random numbers for each difficulty level and in the range of number of squaures:
 const easySquares = 100
@@ -76,40 +70,28 @@ const hardSquares = 49
 
 function randomNumbers(totalSquares) {
     const numbers = [];
-    for (let i = 0; i < 16; i++) {
+
+    while (numbers.length < 16) {
         const randomNumber = Math.floor(Math.random() * totalSquares) + 1;
+
+        // Check if the random number is not already in the array
         if (!numbers.includes(randomNumber)) {
             numbers.push(randomNumber);
         }
     }
+
     return numbers;
 }
 
-//display the numbers in a span in html 
+
+
 function displayBombNumbers(numbers) {
     const numberBombs = document.getElementById('numBombs');
-    numberBombs.innerHTML = ""; // Clear previous bomb numbers
-
-    for (let i = 0; i < numbers.length; i++) {
-        const number = numbers[i];
-        const numberElement = document.createElement('span');
-        numberElement.innerHTML = `${number} `;
-        numberBombs.appendChild(numberElement);
-    }
-
+    numberBombs.innerHTML = "Bombs: " + numbers.join(" ");
+    console.log(numbers)
 }
 
 
-
-
-
-
-
-
-
-
-
-//
 
 
 
